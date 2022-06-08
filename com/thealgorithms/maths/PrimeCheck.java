8de81d692dc3a9b81fe8e2073facbc5e12edb387
Ba9-043 Nguyễn Ngọc Minh <73248681+XDLMAO@users.noreply.github.com>
2022-06-08 10:39:33 +0700

    thealgorithm

---
+++ com/thealgorithms/maths/PrimeCheck.java
@@ -0,0 +1,5 @@
+package com.thealgorithms.maths;
+
+public class PrimeCheck {
+
+}
---
+++ com/thealgorithms/maths/PrimeCheckTest.java
@@ -0,0 +1,24 @@
+package com.thealgorithms.maths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class PrimeCheckTest{
+    @Test
+    public void Test1() {
+        //Valid Partition
+    	assertEquals(true,PrimeCheck.isPrime(3));
+    }
+
+    @Test
+    public void Test2() {
+        //Invalid Partition
+    	assertEquals(false,PrimeCheck.isPrime(4));
+    }
+
+    @Test
+    public void Test3() {
+        //Invalid Partition
+    	assertEquals(false,PrimeCheck.isPrime(-13));
+    }
+
+
+    
